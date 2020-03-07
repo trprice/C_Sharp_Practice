262d957c4606e78038e85f23a3aa75ce874147b3
Taylor Price <taylor@taylorbuiltsolutions.com>
2020-03-07 14:50:41 -0800

    Add project files.

---
+++ StreetAddress.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module1_Assignment
+{
+    class StreetAddress
+    {
+        String Address_FirstLine;
+        String Address_SecondLine;
+        String City;
+        String State;
+        String ZipCode;
+
+
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module1_Assignment
+{
+    class Student
+    {
+        String FirstName;
+        String LastName;
+        DateTime Birthday;
+        
+    }
+}
