7b03024a04a5244c88dadd39b3ccdebe6ae676ff
Taylor Price <trprice@gmail.com>
2020-03-15 17:47:21 -0700

    Add properties to Person.cs to publicly represent the variables

--- Person.cs
+++ Person.cs
@@ -6,9 +6,35 @@ namespace Module1_Assignment
 {
     class Person
     {
-        String FirstName;
-        String LastName;
-        DateTime Birthday;
+        string _first_name;
+        public string FirstName
+        {
+            get { return _first_name; }
+            set { if (value != null) _first_name = value; }
+        }
+
+        string _last_name;
+        public string LastName
+        {
+            get { return _last_name; }
+            set { if (value != null) _last_name = value; }
+        }
+
+        DateTime _birthday;
+        public DateTime Birthday
+        {
+            get { return _birthday; }
+            set
+            {
+                if (value != null)
+                {
+                    // Not checking whether it succeeded because what would we
+                    // do if it wasn't? Throw an exception?
+                    DateTime.TryParse(value.ToString(), out _birthday);
+                }
+            }
+        }
+
         StreetAddress Address;
     }
 }
